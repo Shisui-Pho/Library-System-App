a6b6f6de5118bbdb9c08ea64297c1b7e4e683cf4
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-05-10 12:25:32 +0200

    added book and author models

---
+++ LibrarySystem/LibrarySystem/Models/Author.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibrarySystem.Models;
+
+public class Author
+{
+    [Required]
+    public int Id { get; set; }
+    public bool IsCorporateAuthor { get; set; } = false;
+    [Required]
+    public required string FirstName { get; set; }
+
+    [Required]
+    public required string LastName { get; set; }
+
+    public string Biography { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public DateTime DateOfDeath { get; set; }
+
+    public string Nationality { get; set; }
+
+    public string Website { get; set; }
+
+    // Computed full name
+    public string FullName => $"{FirstName} {LastName}";
+
+    // Navigational property
+    public List<Book> Books { get; set; }
+}
---
+++ LibrarySystem/LibrarySystem/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibrarySystem.Models;
+
+public class Book
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public required string ISBN { get; set; }
+
+    [Required]
+    public required string BookTitle { get; set; }
+
+    public string Description { get; set; }
+
+    [Required]
+    public required string Publisher { get; set; }
+    [Required]
+    public DateTime PublicationDate { get; set; } // When it was published
+
+    public int PageCount { get; set; } // Number of pages
+
+    public string Language { get; set; } // e.g., "en", "fr"
+
+    public string Genre { get; set; } // e.g., "Fantasy", "Science Fiction"
+
+    // Navigational property
+    public List<Author> Authors { get; set; }
+
+    // Computed property for URL-friendly slug
+    public string Slug => BookTitle.Replace(" ", "").ToLowerInvariant();
+}
