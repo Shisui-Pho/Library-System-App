7aaa266737ff4c158bccb10a6a38a28b8799f394
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-05-10 12:32:36 +0200

    configure database connection

--- LibrarySystem/LibrarySystem/Program.cs
+++ LibrarySystem/LibrarySystem/Program.cs
@@ -1,8 +1,13 @@
+using LibrarySystem.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Configure midlle ware
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<AppDBContext>(options =>
+        options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
