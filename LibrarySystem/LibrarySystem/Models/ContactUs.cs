556cea80dcd7df472d070ed65a84ddfc428e4d87
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-05-17 17:46:20 +0200

    Added contact us model

---
+++ LibrarySystem/LibrarySystem/Models/ContactUs.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibrarySystem.Models;
+
+//This is a model that will record all the information that is passed
+// by the user when they want to contact the library.
+public class ContactUs
+{
+    public int Id { get; set; }
+    [Required]
+    public string FullNames { get; set; }
+    [Required]
+    [RegularExpression("/^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$/;")]
+    public string Email {  get; set; }
+    public string PhoneNumber { get; set; }
+    [Required]
+    public string Message {  get; set; }
+}
