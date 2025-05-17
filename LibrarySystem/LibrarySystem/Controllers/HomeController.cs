24cb242a9077dbcb271b4a231118dc3edd588397
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-05-17 20:28:54 +0200

    implemented post action for contact

--- LibrarySystem/LibrarySystem/Controllers/HomeController.cs
+++ LibrarySystem/LibrarySystem/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibrarySystem.Data;
+using LibrarySystem.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LibrarySystem.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly IRepositoryWrapper _repository;
+    public HomeController(IRepositoryWrapper repository)
+    {
+        _repository = repository;
+    }
+
     public IActionResult Index()
     {
         return View();
@@ -13,9 +21,28 @@ public class HomeController : Controller
     {
         return View();
     }
-
+    [HttpGet]
     public IActionResult Contact()
     {
+        //Pass in a new message request
+        return View(new MessageRequest());
+    }
+    [HttpPost]
+    public IActionResult Contact(MessageRequest messageRequest)
+    {
+        //Check the model state
+        if(!ModelState.IsValid)
+        {
+            return View(messageRequest);
+        }
+
+        //-Create a new message request
+        _repository.MessageReuqests.Create(messageRequest);
+        _repository.SaveChanges();
+
+        //-Pass in a temporary messsage to alert the user that the message has bee sent
+        TempData["Message"] = "Your message was sent successfully. A stuff member will reach out to you shortly throug the provided details.";
+        //Return the same view
         return View();
     }
 }
