6729abd6cae45a78cd003b38dce36f821d2b27b0
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-05-17 23:00:49 +0200

    Redirect

---
+++ LibrarySystem/LibrarySystem/Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using LibrarySystem.Data;
+using LibrarySystem.Data.DataAccess;
+using LibrarySystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.Controllers;
+
+public class BooksController : Controller
+{
+    private readonly IRepositoryWrapper _repository;
+    public BooksController(IRepositoryWrapper repositoryWrapper)
+    {
+        _repository = repositoryWrapper;
+    }
+    public IActionResult List()
+    {
+        //Load all the books for now
+        QueryOptions<Book> queryOptions = new()
+        {
+            OrderBy = b => b.BookTitle,
+            OrderByDirection = "asc"
+        };
+
+        IEnumerable<Book> books = _repository.Books.GetWithOptions(queryOptions); 
+        return View(books);
+    }
+    public IActionResult BookDetails(int id)
+    {
+        //this is the id passed
+        Book book =_repository.Books.GetById(id);
+
+        if(book == null)
+        {
+            //Return page not found
+            return RedirectToAction("PageNotFound", "Home", new { message = "Book was not found. It may have been removed from the database." });
+        }
+
+        return View("BookDetails", book);
+    }//BookDetails
+}
