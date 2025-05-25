29641accb96c7cc322f035dc92f9251552d93285
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-05-25 21:20:35 +0200

    mark fields as required

--- LibrarySystem/LibrarySystem/Models/ApplicationUser.cs
+++ LibrarySystem/LibrarySystem/Models/ApplicationUser.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace LibrarySystem.Models;
 
 public class ApplicationUser : IdentityUser
 {
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
 }
