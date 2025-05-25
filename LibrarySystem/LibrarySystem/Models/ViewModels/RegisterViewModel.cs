d71c78e806b1e0f06b8bf3ed90246997d00533c6
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-05-25 19:52:30 +0200

    Added a model for registration

---
+++ LibrarySystem/LibrarySystem/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibrarySystem.Models.ViewModels;
+
+public class RegisterViewModel
+{
+    [Required]
+    [MinLength(1)]
+    [DisplayName("First Name")]
+    public string FirstName { get; set; }
+    [Required]
+    [MinLength(1)]
+    [DisplayName("Last Name")]
+    public string LastName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [Length(2,100, ErrorMessage ="The length of the password must be between 2 and 100 characters")]
+    public string Password { get; set; }
+    [Required]
+    public string ConfirmPassword { get; set; }
+    public string ContactNumber { get; set; } 
+}
