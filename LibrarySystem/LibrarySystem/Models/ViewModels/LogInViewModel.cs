0de1b3c1d0b9fe752247591816978eed82bd4586
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-05-25 19:42:46 +0200

    added logIn viewModel

---
+++ LibrarySystem/LibrarySystem/Models/ViewModels/LogInViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace LibrarySystem.Models.ViewModels;
+public class LogInViewModel
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+}
