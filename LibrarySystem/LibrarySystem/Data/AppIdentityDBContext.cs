03edc4014b69d1588e7320bb9b5ae7934df49ebb
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-05-25 21:13:32 +0200

    add dbcontext for identity users

---
+++ LibrarySystem/LibrarySystem/Data/AppIdentityDBContext.cs
@@ -0,0 +1,10 @@
+﻿using LibrarySystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySystem.Data;
+
+public class AppIdentityDBContext(DbContextOptions<AppIdentityDBContext> options) 
+             : IdentityDbContext<ApplicationUser>(options)
+{
+}
