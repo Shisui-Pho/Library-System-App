44c747d82c03c05b6f3dd1d13aac44a36229b038
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-05-26 19:09:34 +0200

    added script for popu-up

---
+++ LibrarySystem/LibrarySystem/wwwroot/styles/js/login.js
@@ -0,0 +1,7 @@
+﻿document.querySelector("#LogIn").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active")
+});
+
+document.querySelector(".button-close .btn-close").addEventListener("click", function () {
+    document.querySelector(".popup").classList.remove("active")
+});
