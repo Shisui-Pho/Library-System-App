22e00d91f638d2ab439bc200cf87f7967c6e2bd4
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-06-10 21:27:31 +0200

    Added a book Order model

---
+++ LibrarySystem/LibrarySystem/Models/BookOrder.cs
@@ -0,0 +1,16 @@
+﻿namespace LibrarySystem.Models;
+public class BookOrder
+{
+    public int Id { get; set; }
+    public int BookID { get; set; }
+    public int UserID { get; set; }
+    public string Description { get; set; }
+    public decimal TotalPrice => 5m;
+    public int Quantity { get; set; }
+    public DateTime OrderPlacedDate { get; set; }
+    public string OrderStatus { get; set; }
+
+    //Naviagtion properties
+    public Book Book { get; set; }
+    public ApplicationUser User { get; set; }
+}
