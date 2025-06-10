acb91ce38ad444101ad4f0cefe92de24feba6960
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-06-10 21:34:46 +0200

    Added an order model

--- LibrarySystem/LibrarySystem/Models/BookOrder.cs
+++ LibrarySystem/LibrarySystem/Models/BookOrder.cs
@@ -1,4 +1,6 @@
-﻿namespace LibrarySystem.Models;
+﻿using LibrarySystem.Enums;
+
+namespace LibrarySystem.Models;
 public class BookOrder
 {
     public int Id { get; set; }
@@ -8,7 +10,7 @@ public class BookOrder
     public decimal TotalPrice => 5m;
     public int Quantity { get; set; }
     public DateTime OrderPlacedDate { get; set; }
-    public string OrderStatus { get; set; }
+    public BookOrderStatus Status{ get; set; }
 
     //Naviagtion properties
     public Book Book { get; set; }
