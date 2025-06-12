317db15260ce5a84a064a0b5adf6e74e87f959aa
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-06-12 20:24:53 +0200

    Added a view model for the cart

---
+++ LibrarySystem/LibrarySystem/Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace LibrarySystem.Models.ViewModels;
+public class CartViewModel
+{
+    public IEnumerable<BookOrder> BookOrders { get; set; }
+    public int SubTotalItems => BookOrders?.Select(x => x.Quantity).Sum() ?? 0;
+    public decimal CalculateTotalCartPriceWithDiscount(double discountPercentage = 0)
+    {
+        //Calculate the total price
+        decimal totalPrice = CalculateTotalPrice();
+
+        //Apply the discount
+        totalPrice -= totalPrice * (decimal)discountPercentage;
+
+        //returnt the discount price
+        return totalPrice;
+    }
+    public decimal CalculateTotalPrice()
+    {
+        return BookOrders.Select(x =>x.TotalPrice).Sum();
+    }
+}//class
