16a76cbb0875c22f0bc2a09b222f9a3861ec010c
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-06-13 21:07:24 +0200

    created a lightweight cart model

---
+++ LibrarySystem/LibrarySystem/Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibrarySystem.Models;
+public class CartItem
+{
+    public int CartItemID { get; set; }
+    [ForeignKey(nameof(Book))]
+    public int BookID {  get; set; }
+
+    //Navigational property
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
