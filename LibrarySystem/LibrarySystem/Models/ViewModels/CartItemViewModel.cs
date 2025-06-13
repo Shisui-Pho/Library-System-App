6f174b8598feece58de8ef5e8518d6b7c80346d5
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-06-13 21:20:01 +0200

    added product viewmodel

---
+++ LibrarySystem/LibrarySystem/Models/ViewModels/CartItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LibrarySystem.Models.ViewModels;
+public class CartItemViewModel
+{
+    public CartItem CartItem { get; set; }
+
+    //This will be the page that the user clicked on to add item to cart
+    //-The item will be linked to the fragment labeled: product-id
+    public string PageWhereItemWasAdded { get; set; }
+
+}
