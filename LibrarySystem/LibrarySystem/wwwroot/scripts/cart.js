f0bc3967c3f7c172f3a35362456422d71db53961
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-06-19 22:06:45 +0200

    add JQuery script to dynamically update cart count

--- LibrarySystem/LibrarySystem/wwwroot/scripts/cart.js
+++ LibrarySystem/LibrarySystem/wwwroot/scripts/cart.js
@@ -1,1 +1,17 @@
-﻿
+﻿$(function () {
+    $(document).on('click', '.ajax-cart-btn', function () {
+        const button = $(this);
+        const form = button.closest('form');
+        const formData = form.serialize();
+
+        $.post(form.attr('action'), formData, function (response) {
+            $('.cart-count').text(response.cartCount);
+            //button.removeClass('button-blue')
+                //.addClass('button-red')
+                //.text('Remove from cart')
+                //.attr('data-action', 'remove');
+        }).fail(function () {
+            alert("Something went wrong. Try again.");
+        });
+    });
+});
