3959981037569b5f495d2fbf0dcea563649b2252
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-06-26 10:40:22 +0200

    update cart item to make use of CartItem instead of BookOrder

--- LibrarySystem/LibrarySystem/Models/ViewModels/CartViewModel.cs
+++ LibrarySystem/LibrarySystem/Models/ViewModels/CartViewModel.cs
@@ -1,8 +1,8 @@
 ﻿namespace LibrarySystem.Models.ViewModels;
 public class CartViewModel
 {
-    public IEnumerable<BookOrder> BookOrders { get; set; }
-    public int SubTotalItems => BookOrders?.Select(x => x.Quantity).Sum() ?? 0;
+    public IEnumerable<CartItem> CartItems { get; set; }
+    public int SubTotalItems => CartItems?.Select(x => x.Quantity).Sum() ?? 0;
     public decimal CalculateTotalCartPriceWithDiscount(double discountPercentage = 0)
     {
         //Calculate the total price
@@ -16,6 +16,7 @@ public class CartViewModel
     }
     public decimal CalculateTotalPrice()
     {
-        return BookOrders.Select(x =>x.TotalPrice).Sum();
+        decimal price = CartItems.Select(x => x.Quantity * x.Price).Sum();
+        return price;
     }
 }//class
