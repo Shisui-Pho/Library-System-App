efe26431a50ce94cbc763f909125c22b79081a92
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-06-26 16:45:45 +0200

    Add user service class

---
+++ LibrarySystem/LibrarySystem/Infrastructure/UserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace LibrarySystem.Infrastructure;
+public class UserService : IUserService
+{
+    public bool IsLoggedIn(ClaimsPrincipal user)
+    {
+        return user?.Identity != null && user.Identity.IsAuthenticated;
+    }
+    public string GetUserId(ClaimsPrincipal user)
+    {
+        return user?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    }
+}//class
