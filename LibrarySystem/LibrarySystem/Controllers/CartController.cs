19139b680da8e72d74b4d04d8a498dd7c81a4e90
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-06-26 16:51:32 +0200

    use cart service

--- LibrarySystem/LibrarySystem/Controllers/CartController.cs
+++ LibrarySystem/LibrarySystem/Controllers/CartController.cs
@@ -1,91 +1,29 @@
-﻿using LibrarySystem.Data;
-using LibrarySystem.Infrastructure;
-using LibrarySystem.Models;
+﻿using LibrarySystem.Infrastructure;
 using LibrarySystem.Models.ViewModels;
-using LibrarySystem.Utils;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibrarySystem.Controllers;
 public class CartController : Controller
 {
-    private readonly IRepositoryWrapper _repository;
-    public CartController(IRepositoryWrapper repo)
+    private readonly ICartService _cartService;
+    public CartController(ICartService cartService)
     {
-        this._repository = repo;
+        this._cartService = cartService;
     }
     [HttpPost]
     public IActionResult AddToCart(CartItemViewModel model)
     {
-        //-Here if the user is authenticated, we will save the cart info to the database,
-        //  otherwise we use session session based interaction
-
-        int totalCartItems = User.Identity.IsAuthenticated ? 
-            AddToCartViaDatabase(model) : AddToCartViaSession(model);
-
+        var (success, totalCartItems) = _cartService.AddToCart(HttpContext, model);
         return Json(new 
         { 
-            success = true,
+            success = success,
             cartCount = totalCartItems 
         });
-    }
-    [Authorize]
-    private int AddToCartViaDatabase(CartItemViewModel cartInfo)
-    {
-        //For now
-        throw new NotImplementedException();
-    }
-    private int AddToCartViaSession(CartItemViewModel cartInfo)
-    {
-        //-Here if the user is authenticated, we will save the cart info to the database,
-        //  otherwise we use session session based interaction
-        List<CartItem> cart = HttpContext.Session.GetObjectFromJson<List<CartItem>>("Cart") ?? [];
-        
-        var item = cartInfo.CartItem;
-        var existingItem = cart.FirstOrDefault(c => c.BookID == item.BookID);
-
-        if (existingItem != null)
-        {
-            existingItem.Quantity += item.Quantity;//increament the quantity of the item
-        }
-        else
-        {
-            //Add the item
-            cart.Add(item);
-        }
-
-        //Add to the current user session objects
-        HttpContext.Session.SetObjectAsJson("Cart", cart);
-
-        return cart.Count;
     }
     [HttpGet]
     public IActionResult Index()
     {
-        //Retrieve all cart items
-        var cart = new CartViewModel();
-        if (User.Identity.IsAuthenticated)
-        {
-            //Get it from database
-            cart.CartItems = GetCartFromDB();
-        }
-        else
-        {
-            var lst = HttpContext.Session.GetObjectFromJson<List<CartItem>>("Cart") ?? [];
-            //Populate the navigational property
-            cart.CartItems = AddBookAndDetails(lst);
-        }
-        
+        var cart = _cartService.GetCart(HttpContext);
         return View(cart);
     }//Index
-    private IEnumerable<CartItem> AddBookAndDetails(IEnumerable<CartItem> cartItems)
-    {
-        return cartItems.ApplyBookDetails(b => _repository.Books.GetBookWithAuthors(b.BookID));
-    }//AddBookAndDetails
-    
-    [Authorize]
-    private List<CartItem> GetCartFromDB()
-    {
-        throw new NotImplementedException();
-    }
 }//class
