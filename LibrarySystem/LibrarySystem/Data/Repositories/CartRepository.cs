4a0f065c7e77612130b5d319e2ef51497021078f
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-06-27 12:31:23 +0200

    Added cart repository

---
+++ LibrarySystem/LibrarySystem/Data/Repositories/CartRepository.cs
@@ -0,0 +1,19 @@
+﻿using LibrarySystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySystem.Data.Repositories;
+
+public class CartRepository(AppDBContext context) : BaseRepository<CartItem>(context), IBaseRepository<CartItem>, ICartRepository
+{
+    public IEnumerable<CartItem> GetCartItemsOfUser(string userID)
+    {
+        return base._dbContext.CartItems.Where(c => c.UserID == userID)
+                                        .Include(c => c.BookInCart);
+    }//GetCartOfBookss
+
+    public IEnumerable<CartItem> GetCartOfBookss(int bookId)
+    {
+        return base._dbContext.CartItems.Where(c => c.BookID == bookId)
+                                .Include(c => c.BookInCart);
+    }//GetCartOfBookss
+}//class
---
+++ LibrarySystem/LibrarySystem/Data/Repositories/ICartRepository.cs
@@ -0,0 +1,8 @@
+﻿using LibrarySystem.Models;
+
+namespace LibrarySystem.Data.Repositories;
+public interface ICartRepository : IBaseRepository<CartItem>
+{
+    IEnumerable<CartItem> GetCartItemsOfUser(string userID);
+    IEnumerable<CartItem> GetCartOfBookss(int bookId);
+}//
