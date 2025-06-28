e4af7c09bbad8d6e71055b2140c5f959c4a1def6
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-06-28 14:14:59 +0200

    removed unused implementation

--- LibrarySystem/LibrarySystem/Models/ViewModels/CartViewModel.cs
+++ LibrarySystem/LibrarySystem/Models/ViewModels/CartViewModel.cs
@@ -1,21 +1,9 @@
-﻿using System.Linq;
-
-namespace LibrarySystem.Models.ViewModels;
+﻿namespace LibrarySystem.Models.ViewModels;
 public class CartViewModel
 {
     public IEnumerable<CartItem> CartItems { get; set; }
     public int SubTotalItems => CartItems?.Select(x => x.Quantity).Sum() ?? 0;
-    public decimal CalculateTotalCartPriceWithDiscount(double discountPercentage = 0)
-    {
-        //Calculate the total price
-        decimal totalPrice = CalculateTotalPrice();
 
-        //Apply the discount
-        totalPrice -= totalPrice * (decimal)discountPercentage;
-
-        //returnt the discount price
-        return totalPrice;
-    }
     public decimal CalculateTotalPrice()
     {
         decimal price = CartItems.Select(x => x.Quantity * x.Price).Sum();
