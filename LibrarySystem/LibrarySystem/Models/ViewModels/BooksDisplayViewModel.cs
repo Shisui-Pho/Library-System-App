58f5ca986cda1e529eebb3b691157ae74ad4b4cf
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-06-28 14:14:05 +0200

    added method to generate unique identifire for each book elements

--- LibrarySystem/LibrarySystem/Models/ViewModels/BooksDisplayViewModel.cs
+++ LibrarySystem/LibrarySystem/Models/ViewModels/BooksDisplayViewModel.cs
@@ -3,4 +3,8 @@ public class BooksDisplayViewModel
 {
     public IEnumerable<Book> Books { get; set; }
     public PagingInfomation PagingInfomation { get; set; }
+    public string GenerateUniqueIdentifier(int bookID, bool isForAddToChart = false)
+    {
+        return isForAddToChart ? "addbookNumber" + bookID : "updatebookNumber" + bookID;
+    }
 }
