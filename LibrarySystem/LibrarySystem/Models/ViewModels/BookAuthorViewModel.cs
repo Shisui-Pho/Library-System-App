e1e399f3499d07f3faad7ffda7e81717eba37730
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-07-09 18:51:51 +0200

    added author details with a list of books view

---
+++ LibrarySystem/LibrarySystem/Models/ViewModels/BookAuthorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LibrarySystem.Models.ViewModels;
+public class BookAuthorViewModel(List<(int bookId, int quantity)> items)
+{
+    public IEnumerable<Book> Books { get; set; }
+    public Author Author { get; set; }
+    public int NumberOfBooksByAuthor => Books?.Count() ?? 0;
+    public CartItemStatus CartItemStatus { get; } = new CartItemStatus(items);
+}
