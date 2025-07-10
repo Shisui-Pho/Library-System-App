124dcc2ea65194a64f72e624a23b1dcc7c3140df
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-07-10 20:30:48 +0200

    added an attribute class

---
+++ LibrarySystem/LibrarySystem/Infrastructure/RequiredIfAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibrarySystem.Infrastructure;
+public class RequiredIfAttribute : ValidationAttribute, IClientModelValidator
+{
+    private string PropertyName { get; set; }
+    private object DesiredValue { get; set; }
+
+    public RequiredIfAttribute(string propertyName, object desiredValue)
+    {
+        PropertyName = propertyName;
+        DesiredValue = desiredValue;
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext context)
+    {
+        var instance = context.ObjectInstance;
+        var type = instance.GetType();
+        var propertyValue = type.GetProperty(PropertyName)?.GetValue(instance, null);
+
+        if (propertyValue?.ToString() == DesiredValue.ToString() && value == null)
+        {
+            return new ValidationResult(ErrorMessage);
+        }
+        return ValidationResult.Success;
+    }
+
+    public void AddValidation(ClientModelValidationContext context)
+    {
+        context.Attributes.Add("data-val", "true");
+        context.Attributes.Add("data-val-requiredif", ErrorMessage);
+        context.Attributes.Add("data-val-requiredif-property", PropertyName);
+        context.Attributes.Add("data-val-requiredif-desiredvalue", DesiredValue.ToString());
+    }
+}
