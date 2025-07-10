0f77f695365210c63623c6b3c17a8d7005c6302c
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-07-10 19:00:23 +0200

    add payment method model

---
+++ LibrarySystem/LibrarySystem/Models/PaymentMethod.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibrarySystem.Models;
+public class PaymentMethod
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    [StringLength(255)]
+    public string Description { get; set; }
+
+    public bool IsActive { get; set; } = true;
+}
