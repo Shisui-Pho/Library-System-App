282023b2fcff2a2a6b543b21f264a02644d39fd9
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-07-10 19:00:40 +0200

    add pick-up point model

---
+++ LibrarySystem/LibrarySystem/Models/PickupPoint.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibrarySystem.Models;
+public class PickupPoint
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string Address { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string City { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Province { get; set; }
+
+    [StringLength(20)]
+    public string Phone { get; set; }
+
+    [StringLength(100)]
+    public string ContactPerson { get; set; }
+
+    public TimeSpan OpeningTime { get; set; }
+    public TimeSpan ClosingTime { get; set; }
+
+    public bool IsActive { get; set; } = true; //Active by default
+}//class
