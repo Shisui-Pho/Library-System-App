0e890dc914bfd7899e9278db3cb4417e4d3bb48f
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-07-12 13:40:53 +0200

    made fields nullable

--- LibrarySystem/LibrarySystem/Models/BookOrder.cs
+++ LibrarySystem/LibrarySystem/Models/BookOrder.cs
@@ -2,28 +2,25 @@
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 namespace LibrarySystem.Models;
 public class BookOrder
 {
     public int BookOrderId { get; set; }
     [ForeignKey(nameof(Models.Book))]
-    public int BookID { get; set; }
     public string UserID { get; set; }
     [StringLength(500, ErrorMessage ="Description cannot be more than 500 characters.")]
     public string Description { get; set; }
     [Precision(18, 2)]
     public decimal TotalPrice { get; set; }
-    [Required]
-    [Range(1,10)]//Limit to 10 books per book order.
-    public int Quantity { get; set; }
     public BookOrderStatus Status{ get; set; }
     [Required]
     public DateTime CreatedAt { get; set; }
     public DateTime LastUpdatedAt { get; set; }
 
     //PaymentMethod
-    [ForeignKey(nameof(PaymentMethod))]
+    [ForeignKey(nameof(Models.PaymentMethod))]
     [Required]
     public int PaymentMethodId { get; set; }
 
@@ -32,12 +29,14 @@ public class BookOrder
     public DeliveryOption DeliveryOption { get; set; }
 
     [ForeignKey(nameof(Models.DeliveryAddress))]
-    public int DeliveryAddressId { get; set; }
+    [AllowNull]
+    public int? DeliveryAddressId { get; set; }
     [ForeignKey(nameof(Models.PickupPoint))]
-    public int PickupPointId { get; set; }
+    [AllowNull]
+    public int? PickupPointId { get; set; }
 
-    //Naviagtion properties
-    public Book Book { get; set; }
+    //Navigation properties
+    public ICollection<BookOrderItem> BookOrderItems { get; set; }
 
     //Either DeliveryAddress or PickupPoint must be set, but not both.
     // This is enforced at the application level, not in the database.
