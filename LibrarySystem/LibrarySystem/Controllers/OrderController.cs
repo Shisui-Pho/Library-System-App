282f5f213d505571ddf0510a18a0a3a75b6d93e6
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-07-12 14:46:13 +0200

    rename

---
+++ LibrarySystem/LibrarySystem/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.Controllers;
+[Authorize]
+public class OrderController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult OrderDetails(int orderId)
+    {
+        return View();
+    }//OrderDetails
+}//class
