753b23db686aa313e1207b56be883cfde8c826ba
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-07-15 15:13:33 +0200

    add viewmodels

---
+++ LibrarySystem/LibrarySystem/Models/OrdersDisplayViewModel.cs
@@ -0,0 +1,8 @@
+﻿using LibrarySystem.Models.ViewModels;
+
+namespace LibrarySystem.Models;
+public class OrdersDisplayViewModel
+{
+    public IEnumerable<OrderViewModel> Orders { get; set; }
+    public PagingInfomation PagingInfomation { get; set; }
+}
