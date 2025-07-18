334503cd3ee08ea9a136f7970aa3d896a3ebdfc3
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-07-18 11:55:30 +0200

    explicit property set

--- LibrarySystem/LibrarySystem/Models/ViewModels/BooksDisplayViewModel.cs
+++ LibrarySystem/LibrarySystem/Models/ViewModels/BooksDisplayViewModel.cs
@@ -2,9 +2,18 @@
 namespace LibrarySystem.Models.ViewModels;
 public class BooksDisplayViewModel(List<(int bookId, int quantity)> cartItems)
 {
+    private FilteringOptions _filteringOptions = new FilteringOptions();
     public IEnumerable<Book> Books { get; set; }
     public IEnumerable<string> Genres { get; set; } = ["Horror", "Scifi", "Comedy"];
     public PagingInfomation PagingInfomation { get; set; }
     public CartItemStatus CartItemStatus { get; } = new CartItemStatus(cartItems);
-    public FilteringOptions FilteringOptions { get; set; } = new FilteringOptions();
+    public FilteringOptions FilteringOptions 
+    { 
+        get => _filteringOptions;
+        set
+        {
+            PagingInfomation = value?.Paging ?? new PagingInfomation();
+            _filteringOptions = value;
+        }
+    }//end properyty
 }//class
