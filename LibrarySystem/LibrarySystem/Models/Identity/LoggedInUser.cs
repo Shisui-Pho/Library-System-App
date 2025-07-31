09071cbf3102788b810745c49bc814b725720fb3
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-07-31 22:28:34 +0200

    migration

--- LibrarySystem/LibrarySystem/Models/Identity/LoggedInUser.cs
+++ LibrarySystem/LibrarySystem/Models/Identity/LoggedInUser.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LibrarySystem.Models.Identity;
 public class LoggedInUser
 {
     public int Id { get; set; }
+    [Required]
     [ForeignKey("AspNetUsers")]
     public string UserId { get; set; }
+    [Required]
     public string SessionId { get; set; }
+    [Required]
     public string IPAddress { get; set; }
     public DateTime LoginTime { get; set; }
     public DateTime LastActivity { get; set; }
