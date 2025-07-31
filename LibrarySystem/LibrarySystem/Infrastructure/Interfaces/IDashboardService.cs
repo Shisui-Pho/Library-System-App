85c6b9b9bf03eada3df211ed8efcb23ae1fc73b1
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-07-31 21:03:08 +0200

    added dashboard service

---
+++ LibrarySystem/LibrarySystem/Infrastructure/Interfaces/IDashboardService.cs
@@ -0,0 +1,8 @@
+﻿using LibrarySystem.Models.ViewModels;
+using System.Security.Claims;
+
+namespace LibrarySystem.Infrastructure.Interfaces;
+public interface IDashboardService
+{
+    AdminStaffDashboardViewModel GetDashboardDetailsModel(ClaimsPrincipal user);
+}
---
+++ LibrarySystem/LibrarySystem/Infrastructure/Services/DashboardService.cs
@@ -0,0 +1,27 @@
+﻿using LibrarySystem.Infrastructure.Interfaces;
+using LibrarySystem.Models.ViewModels;
+using System.Security.Claims;
+
+namespace LibrarySystem.Infrastructure.Services;
+public class DashboardService : IDashboardService
+{
+    private readonly IUserService _userService;
+    private readonly IRepositoryWrapper _repo;
+
+    public DashboardService(IRepositoryWrapper repo, IUserService userService)
+    {
+        this._userService = userService;
+        this._repo  = repo;
+    }
+    #nullable enable
+    public AdminStaffDashboardViewModel? GetDashboardDetailsModel(ClaimsPrincipal user)
+    {
+        if (_userService.IsLoggedIn(user))
+            return null;
+
+        return default;
+    }//GetDashboardDetailsModel
+
+    
+
+}//class
