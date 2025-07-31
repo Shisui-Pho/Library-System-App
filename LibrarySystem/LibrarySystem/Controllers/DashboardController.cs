ad380638fd536de0fccb3529150439d8933126cf
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-07-31 20:32:18 +0200

    Added deshboard controller

---
+++ LibrarySystem/LibrarySystem/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.Controllers;
+[Authorize]
+public class DashboardController : Controller
+{
+    [Authorize(Roles ="Administrator")]
+    public IActionResult AdminDashboard()
+    {
+        return View();
+    }
+    [Authorize(Roles ="Staff")]
+    public IActionResult StaffDashboard()
+    {
+        return View();
+    }//StaffDashboard
+    public IActionResult CustomerDashboard()
+    {
+        return View();
+    }//CustomerDashboard
+}//class
