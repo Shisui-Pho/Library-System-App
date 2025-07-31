c72e4546af00df12f92508f2bdc1cd0070d766b5
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-07-31 22:19:42 +0200

    add loggedIn users table

--- LibrarySystem/LibrarySystem/Data/AppIdentityDBContext.cs
+++ LibrarySystem/LibrarySystem/Data/AppIdentityDBContext.cs
@@ -3,8 +3,13 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LibrarySystem.Data;
-
 public class AppIdentityDBContext(DbContextOptions<AppIdentityDBContext> options) 
              : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<LoggedInUser> loggedInUsers { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<LoggedInUser>().ToTable(nameof(LoggedInUser));
+        base.OnModelCreating(builder);
+    }
 }
---
+++ LibrarySystem/LibrarySystem/Models/Identity/LoggedInUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibrarySystem.Models.Identity;
+public class LoggedInUser
+{
+    public int Id { get; set; }
+    [ForeignKey("AspNetUsers")]
+    public string UserId { get; set; }
+    public string SessionId { get; set; }
+    public string IPAddress { get; set; }
+    public DateTime LoginTime { get; set; }
+    public DateTime LastActivity { get; set; }
+    //Navigational property
+    public ApplicationUser User { get; set; }
+}//class
