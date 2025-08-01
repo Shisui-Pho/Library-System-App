5637b306a8f9b51f8a7561270ed0b97ef1370ca3
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2025-08-02 00:16:49 +0200

    add procedure for genres

---
+++ LibrarySystem/LibrarySystem/Data/Database Scripts/Proc_GetTopGenres.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE Proc_GetTopGenres
+			@TopN INT = 5
+AS
+BEGIN
+	SELECT TOP (@TopN) Genre.Name, Count(*) FROM OrderItem
+	JOIN Book
+	ON Book.Id = OrderItem.BookId
+	JOIN BookGenre 
+	ON BookGenre.BooksId = Book.Id
+	JOIN Genre
+	ON Genre.Id = BookGenre.GenresId
+	GROUP BY Genre.Name
+	ORDER BY Count(*) DESC
+
+	-- In the future I may need to add a Union for a second query to this one which will be 
+	  -- based on the wishlist as well as cartlist
+END
+
+
+
